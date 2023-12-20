6508ac45524ee7e0e7585107551aaf5538838d13
Toàn Phạm <tonyorp@TonyProMax14inch.local>
2023-12-20 23:44:45 +0700

    fix: build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  build: {
+    transpile:
+        process.env.NODE_ENV === 'production'
+            ? [
+              'naive-ui',
+              'vueuc',
+              '@css-render/vue3-ssr',
+              '@juggle/resize-observer'
+            ]
+            : ['@juggle/resize-observer']
+  },
   devtools: { enabled: true }
 })
